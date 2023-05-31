d2c565b116f0512fa71dc2b6db821bb92e52d2cf
daichi nakano <soccersaiko45@gmail.com>
2023-05-31 13:29:31 +0900

    Add:会員登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :username, presence: true, length: { maximum: 50, allow_blank: true }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
 end
