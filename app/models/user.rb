6595e795f332ac9157040870e37b33dedaa4cb99
daichi nakano <soccersaiko45@gmail.com>
2023-06-11 18:54:18 +0900

    Add:削除ボタン

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+
+
+  def own?(object)
+    object.user_id == id
+  end
 end
