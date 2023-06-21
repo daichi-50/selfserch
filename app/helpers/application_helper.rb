4b5e9723a2a58deaa4de0cee15cb95e5536b2627
daichi nakano <soccersaiko45@gmail.com>
2023-06-22 05:39:33 +0900

    Fix:OGP画像

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def default_meta_tags
+    def default_meta_tags(post = nil)
         {
             site: '自分、探してます',
-            tilte: '日常の中で失ったものはありませんか？自分見失ってませんか？',
+            title: '日常の中で失ったものはありませんか？自分見失ってませんか？',
             reverse: true,
             separator: '|',
             description: '「自分、探してます」は、見失ったものに関する投稿をします。そしてみんなからコメントをもらうサービスです。',
@@ -14,14 +14,15 @@ module ApplicationHelper
             site_name: '自分、探してます',
             title: :title,
             description: :description,
-            #image: url_for(post.generated_card) if post.generated_card.attached?,
+            image: post&.generated_card&.file&.present? ? post.generated_card.url : image_url("ogp.jpg"),
             type: 'website',
             url: request.original_url,
-            #image: image_url("ogp.jpg"),
-            locale: 'ja_JP'
+            local: 'ja_JP'
             },
             twitter: {
             card: 'summary_large_image',
+            site: '@Nakano-41期生',
+            image: post&.generated_card&.file&.present? ? post.generated_card.url : image_url("ogp.jpg")
             }
         }
     end
