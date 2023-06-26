d28c12d896332b0149ad1adb19d6dc322e4e23a3
daichi nakano <soccersaiko45@gmail.com>
2023-06-26 20:47:45 +0900

    Fix:deviceの調整

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,4 @@
 class TopsController < ApplicationController
-    skip_before_action :require_login
+    skip_before_action :authenticate_user!
     def index; end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  authenticates_with_sorcery!
+        :recoverable, :rememberable, :validatable
+        
   has_many :posts, dependent: :destroy
   has_many :messages, dependent: :destroy
 
   validates :username, presence: true, length: { maximum: 50, allow_blank: true }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
-
 
   def own?(object)
     object.user_id == id
