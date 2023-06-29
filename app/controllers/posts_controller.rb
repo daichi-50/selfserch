06cbd80b5cf45cd8d705a4381aee93459b76ef01
daichi nakano <soccersaiko45@gmail.com>
2023-06-29 21:57:44 +0900

    Fix:N+1問題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
     before_action :set_post, only: [ :edit, :update, :destroy]
 
     def index
-        @posts = Post.page(params[:page])
+        @posts = Post.page(params[:page]).includes(:user).order(created_at: :desc)
     end
 
     def show
