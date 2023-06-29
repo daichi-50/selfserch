c70c8d8ad59286c084374bc2652b720fd60d02a2
daichi nakano <soccersaiko45@gmail.com>
2023-06-29 13:07:49 +0900

    Fix:action_cable.rb

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -3,7 +3,7 @@ Rails.application.config.action_cable.mount_path = '/cable'
 if Rails.env.development?
     Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3000']
 elsif Rails.env.production?
-    Rails.application.config.action_cable.allowed_request_origins = ['https://<your-app-name>.herokuapp.com', 'http://<your-custom-domain>.com']
+    Rails.application.config.action_cable.allowed_request_origins = ['https://selfserch.herokuapp.com', 'http://<your-custom-domain>.com']
 end
 
 Rails.application.config.action_cable.disable_request_forgery_protection = true
