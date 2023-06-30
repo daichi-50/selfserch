ae711977b9a0bb16b6bdb2ea0172876b928f20fb
daichi nakano <soccersaiko45@gmail.com>
2023-07-01 06:25:10 +0900

    Add:user/showにページネーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @posts = @user.posts
+        @posts = @user.posts.page(params[:page]).per(3).order(created_at: :desc)
     end
 
     def search
