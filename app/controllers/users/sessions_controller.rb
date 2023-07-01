9d08c16fa2a8b1975585519fc1403f6a150f5adb
daichi nakano <soccersaiko45@gmail.com>
2023-07-01 17:36:17 +0900

    Fix:チャットのcss

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,6 @@ class Users::SessionsController < ApplicationController
     def guest_sign_in
         user = User.guest
         sign_in user
-        redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+        redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
     end
 end
