78d27719ae44b0bb96df385ed0866af1cf0fda56
daichi nakano <soccersaiko45@gmail.com>
2023-07-02 14:23:49 +0900

    Add:breadcrumbを追加

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,44 @@
+# breadcrumbs.rb
+crumb :root do
+  link "TOP", root_path
+end
+
+crumb :user_new do
+  link "登録", new_user_registration_path
+  parent :root
+end
+
+crumb :user_sign_in do
+  link "ログイン", new_user_session_path
+  parent :user_new
+end
+
+crumb :user_show do |user|
+  link "#{user.username}さんのページ", user_path(user)
+  parent :root
+end
+
+crumb :user_index do
+  link "懸賞金獲得ランキング", users_path
+  parent :post_index
+end
+
+crumb :user_search do
+  link "懸賞金を贈る", search_users_path
+  parent :post_show
+end
+
+crumb :post_index do
+  link "手配書一覧", posts_path
+  parent :root
+end
+
+crumb :post_new do
+  link "手配書作成", new_post_path
+  parent :post_index
+end
+
+crumb :post_show do |post|
+  link "手配書", post_path(post)
+  parent :post_index
+end
