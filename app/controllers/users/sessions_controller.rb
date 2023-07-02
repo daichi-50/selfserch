d0122ee27eb5fe1c14d80bf224892796cb2807be
daichi nakano <soccersaiko45@gmail.com>
2023-07-02 09:24:12 +0900

    Fix:flashメッセージと国際化

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,6 @@ class Users::SessionsController < ApplicationController
     def guest_sign_in
         user = User.guest
         sign_in user
-        redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
+        redirect_to posts_path, flash: { success: t('.success') }
     end
 end
