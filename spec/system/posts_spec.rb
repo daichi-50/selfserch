454741c20cf4245a58781f0c1286661a47401a5c
daichi nakano <soccersaiko45@gmail.com>
2023-07-06 08:29:02 +0900

    Fix:ボタンの色

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
-  let(:post_create_user){ create(:user) }
-  before do
-    driven_by(:rack_test)
-    @user = FactoryBot.create(:user)
-  end
+  let(:user) { create(:user) }
+  before { sign_in user }
     describe "手配書作成" do
       context "有効な値を入力したとき" do
         it "手配書作成が成功する" do
-          sign_in @user
           visit new_post_path
 
           attach_file "画像", Rails.root.join('spec/fixtures/test_image.png')
@@ -23,14 +19,64 @@ RSpec.describe 'Posts', type: :system do
       end
       context "画像が未選択のとき" do
         it "手配書作成が失敗する" do
-          sign_in @user
           visit new_post_path
 
           fill_in "探しもの", with: "test_title"
           fill_in "詳細", with: "test_description"
           click_button "投稿する"
 
-          expect(page).to have_current_path(new_post_path)
+          expect(page).to have_content ("画像を入力してください")
+        end
+      end
+      context "探しものが未入力のとき" do
+        it "手配書作成が失敗する" do
+          visit new_post_path
+
+          attach_file "画像", Rails.root.join('spec/fixtures/test_image.png')
+          fill_in "詳細", with: "test_description"
+          click_button "投稿する"
+
+          expect(page).to have_content ("探しものは1文字以上で入力してください")
+        end
+      end
+      context "詳細が未入力のとき" do
+        it "手配書作成が失敗する" do
+          visit new_post_path
+
+          attach_file "画像", Rails.root.join('spec/fixtures/test_image.png')
+          fill_in "探しもの", with: "test_title"
+          click_button "投稿する"
+
+          expect(page).to have_content ("詳細は1文字以上で入力してください")
+        end
+      end
+    end
+    describe "手配書の閲覧" do
+      let(:post) do 
+        user.posts.create!(
+          :title => "test_title",
+          :description => "test_description",
+          :image => Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.png'), 'image/png'),
+          :prize_money => 1000,
+          :generated_card => Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_card_image.png'), 'image/png')
+        )
+      end
+      context "手配書詳細画面に遷移したとき" do
+        it "手配書詳細画面に遷移する" do
+          visit post_path(post)
+
+          expect(page).to have_current_path(post_path(post))
+          expect(page).to have_content ("削除")
+        end
+      end
+      context "手配書詳細画面で削除ボタンを押したとき" do
+        it "手配書が削除される" do
+          visit post_path(post)
+
+          click_on "削除"
+
+          expect(page).to have_current_path(posts_path)
+          expect(page).to have_content ("手配書を削除しました")
         end
       end
     end
